5973fd3400e7929a4ea11b6db959f80c62ae15f0
Kris Jeni <jenistar90@gmail.com>
2015-01-20 20:52:51 +0700

    #9

--- application/views/page/vwHowItWorks.php
+++ application/views/page/vwHowItWorks.php
@@ -8,17 +8,17 @@
     <?php $this->load->view('customer/vwHeader'); ?>
     <main style="background: url(/assets/images/how_it_works_background.jpg); background-size: cover;">
         <div class="container" style="font-family: 'Bitter';">
-            <div class="row margin-top-30">
+            <div class="row padding-top-50">
                 <h1 class="text-center color-blue">How it works?</h1>
             </div>
             
-            <div class="row margin-top-30">
-                <div class="col-sm-2 col-sm-offset-1">
+            <div class="row padding-top-50">
+                <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
                     1
                     </div>
                 </div>
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h2 class="color-blue">Kick Off</h2>
                     <p class="font-droid">Download our mobile app for use</p>
                     <p class="font-droid">our Web System to get started</p>
@@ -26,12 +26,12 @@
             </div>
             
             <div class="row margin-top-20">
-                <div class="col-sm-2 col-sm-offset-1">
+                <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
                     2
                     </div>
                 </div>
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h2 class="color-blue">Choose the lucky recipient</h2>
                     <p class="font-droid">Putting the information of the beloved you would like to give</p>
                     <p class="font-droid">gift for in the system. Decide the value size of the gift to pool</p>
@@ -40,12 +40,12 @@
             </div>
             
             <div class="row margin-top-20">
-                <div class="col-sm-2 col-sm-offset-1">
+                <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
                     3
                     </div>
                 </div>
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h2 class="color-blue">Invite other friends to contribute</h2>
                     <p class="font-droid">Select the other friends to create the suprise together by</p>
                     <p class="font-droid">putting their information in the Web system, or simply choose</p>
@@ -55,12 +55,12 @@
             </div>
             
             <div class="row margin-top-20">
-                <div class="col-sm-2 col-sm-offset-1">
+                <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
                     4
                     </div>
                 </div>
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h2 class="color-blue">Fundraise for the surprise</h2>
                     <p class="font-droid">Your friends can simultaneously confirm their participation</p>
                     <p class="font-droid">and contribute their shares by simply replying "OK" by SMS to</p>
@@ -71,12 +71,12 @@
             </div>
             
             <div class="row margin-top-20">
-                <div class="col-sm-2 col-sm-offset-1">
+                <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
                     5
                     </div>
                 </div>
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <h2 class="color-blue">Send the love</h2>
                     <p class="font-droid">One the money is collected; we will deliver the SMS gift</p>
                     <p class="font-droid">notification on your behalf to the recipient. Afterwards, the</p>
