e3048e5033acfd0985ad3f3afb272043f62fdf35
Kris Jeni <jenistar90@gmail.com>
2015-01-28 00:46:08 +0700

    How It Works!

--- application/views/page/vwHowItWorks.php
+++ application/views/page/vwHowItWorks.php
@@ -9,81 +9,81 @@
     <main style="background: url(/assets/images/how_it_works_background.jpg); background-size: cover;">
         <div class="container" style="font-family: 'Bitter';">
             <div class="row padding-top-50">
-                <h1 class="text-center color-blue">How it works?</h1>
+                <h1 class="text-center color-blue">HOW IT WORKS</h1>
             </div>
             
             <div class="row padding-top-50">
                 <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
-                    1
+                        <img src="<?php echo base_url()."assets/images/icon_white_01.png";?>" style="width: 50%;"/>
                     </div>
                 </div>
-                <div class="col-sm-7">
-                    <h2 class="color-blue">Kick Off</h2>
-                    <p class="font-droid">Download our mobile app for use</p>
-                    <p class="font-droid">our Web System to get started</p>
+                <div class="col-sm-7 color-gray-middle" style="font-weight: bold;">
+                    <h2>1. Kick Off</h2>
+                    <p class="font-droid">
+                        Create group gift project by downloading our mobile app or using 
+                        <br/>our web app
+                    </p>
                 </div>
             </div>
             
             <div class="row margin-top-20">
                 <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
-                    2
+                        <img src="<?php echo base_url()."assets/images/icon_white_02.png";?>" style="width: 50%;"/>
                     </div>
                 </div>
-                <div class="col-sm-7">
-                    <h2 class="color-blue">Choose the lucky recipient</h2>
-                    <p class="font-droid">Putting the information of the beloved you would like to give</p>
-                    <p class="font-droid">gift for in the system. Decide the value size of the gift to pool</p>
-                    <p class="font-droid">the money.</p>
+                <div class="col-sm-7 color-gray-middle">
+                    <h2>2. Invite friends</h2>
+                    <p class="font-droid">
+                        Select friends from your phone contact book and invite them to join 
+                        <br/>via SMS
+                    </p>
                 </div>
             </div>
             
             <div class="row margin-top-20">
                 <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
-                    3
+                        <img src="<?php echo base_url()."assets/images/icon_white_03.png";?>" style="width: 50%;"/>
                     </div>
                 </div>
                 <div class="col-sm-7">
-                    <h2 class="color-blue">Invite other friends to contribute</h2>
-                    <p class="font-droid">Select the other friends to create the suprise together by</p>
-                    <p class="font-droid">putting their information in the Web system, or simply choose</p>
-                    <p class="font-droid">from your phone contact lists. Our system will automatically</p>
-                    <p class="font-droid">send them SMS invitations to join your project.</p>
+                    <h2>3. Collect money</h2>
+                    <p class="font-droid">
+                        Friends confirm and contribute money by replying "OK" to our SMS
+                        <br/> system
+                    </p>
                 </div>
             </div>
             
             <div class="row margin-top-20">
                 <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
-                    4
+                        <img src="<?php echo base_url()."assets/images/icon_white_04.png";?>" style="width: 50%;"/>
                     </div>
                 </div>
                 <div class="col-sm-7">
-                    <h2 class="color-blue">Fundraise for the surprise</h2>
-                    <p class="font-droid">Your friends can simultaneously confirm their participation</p>
-                    <p class="font-droid">and contribute their shares by simply replying "OK" by SMS to</p>
-                    <p class="font-droid">our system. Money for your gift-giving project will be collected</p>
-                    <p class="font-droid">automatically since it will be added to your friends' next phone</p>
-                    <p class="font-droid">bills</p>
+                    <h2>4. Spend money</h2>
+                    <p class="font-droid">
+                        Choose a gift from our selection, let your recipient choose the gift or
+                        <br/> withdraw the money
+                    </p>
                 </div>
             </div>
             
             <div class="row margin-top-20">
                 <div class="col-sm-2 col-sm-offset-2">
                     <div class="how-it-work-step-blue">
-                    5
+                        <img src="<?php echo base_url()."assets/images/icon_white_05.png";?>" style="width: 50%;"/>
                     </div>
                 </div>
                 <div class="col-sm-7">
-                    <h2 class="color-blue">Send the love</h2>
-                    <p class="font-droid">One the money is collected; we will deliver the SMS gift</p>
-                    <p class="font-droid">notification on your behalf to the recipient. Afterwards, the</p>
-                    <p class="font-droid">recipient can freely choose how to spend the money by</p>
-                    <p class="font-droid">choosing from a huge variety of gifts in our system. They can</p>
-                    <p class="font-droid">select either online gift card code to redeem online or physical</p>
-                    <p class="font-droid">gift card sent via post.</p>
+                    <h2>5. Give the love</h2>
+                    <p class="font-droid">
+                        Mission completed. Send the gift to your beloved and make them
+                        <br/> tounched
+                    </p>
                 </div>
             </div>
             <div class="margin-top-50">&nbsp;</div>
