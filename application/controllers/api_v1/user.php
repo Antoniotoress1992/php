ca582a95761a62821f64085a6aeb72ba9ed6a301
Kris Jeni <jenistar90@gmail.com>
2015-02-13 00:04:09 +0700

    Profile

--- application/controllers/api_v1/user.php
+++ application/controllers/api_v1/user.php
@@ -39,5 +39,26 @@ class User extends CI_Controller {
         $result = $this->user_model->signin($phone, $password);
     
         die(json_encode($result));
+    }
+    
+    public function detail() {
+        $this->load->model('user_model');
+        $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
+        $result = $this->user_model->detail($user_id);
+        die(json_encode($result));        
+    }
+
+    public function update() {
+        $this->load->model('user_model');
+        $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+        $name = isset($_POST['name']) ? $_POST['name'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+        $country_id = isset($_POST['country_id']) ? $_POST['country_id'] : '';
+        
+        $result = $this->user_model->update($user_id, $name, $password, $email, $phone, $country_id);
+        die(json_encode($result));
     }    
+    
 }
